546ff2cb8a31bd0b25273b53c5ec14e2d5113ebf
Yuneider <yparada22@gmail.com>
2021-03-26 19:47:06 -0500

    Inicial

---
+++ Burbuja/src/burbuja/Burbuja.java
@@ -0,0 +1,34 @@
+package burbuja;
+
+public class Burbuja {
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6};
+        int[] b = {6,5,4,3,2,1};
+        int n = b.length;
+        int t; 
+        int cont =0;
+        cont+=2;
+        for(int i=0;i<(n-1);i++){
+            cont+=6; 
+            for(int j=i+1;j<n;j++){
+                cont+=6;
+                if(b[i]<b[j]){
+                    cont+=7;
+                    t=b[i];
+                    b[i]=b[j];
+                    b[j]=t;
+                }
+            }
+        }
+        //OD -> Ordenado descendente
+        //OA -> Ordenado Ascendete
+        //A  -> Aleatoriamente
+        int formulaOD = 2+6*(n-1)+6*((n*n-n)/2);
+        int formulaOA = 2+6*(n-1)+13*((n*n-n)/2);
+        int formulaA;
+        System.out.println("Formula: "+formulaOD);
+        System.out.println("Contador: "+cont);
+    }
+    
+}
