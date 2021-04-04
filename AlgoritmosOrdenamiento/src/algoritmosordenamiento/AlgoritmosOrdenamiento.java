39eab702c0604ab90a1e2879a866617311b4ff14
Yuneider <yparada22@gmail.com>
2021-04-04 01:15:11 -0500

    Solo falta Hacer que las gráficas se vean mejor

--- AlgoritmosOrdenamiento/src/algoritmosordenamiento/AlgoritmosOrdenamiento.java
+++ AlgoritmosOrdenamiento/src/algoritmosordenamiento/AlgoritmosOrdenamiento.java
@@ -7,12 +7,14 @@ public class AlgoritmosOrdenamiento {
     static Ordenar orden = new Ordenar();
     
     public static void main(String[] args) {
-        int[] a = {35,62,56,3,16};
-        System.out.println("Arreglo Original:");
-        orden.MostrarArreglo(a);
+        int[] a = {135,262,356,3,1};
+        int[] b = {92,250,1000};
+        int[] c = {500,250,140,34,21,1};
         orden.Radix(a);
-        //VentanaDescendente vd = new VentanaDescendente();
-        //vd.getClass();
+        orden.Radix(b);
+        orden.Radix(c);
+        VentanaDescendente vd = new VentanaDescendente();
+        vd.getClass();
     }
     
 }
--- AlgoritmosOrdenamiento/src/algoritmosordenamiento/Ordenar.java
+++ AlgoritmosOrdenamiento/src/algoritmosordenamiento/Ordenar.java
@@ -74,28 +74,56 @@ public class Ordenar {
     public void Radix(int a[]){
         System.out.println("Ordenamiento por Radix:");
         int max = GetMax(a);
+        int cont=0 ,x=0, w1=0 , w2=0;
+        //w1 es la cantidad de dígitos del mayor número del arreglo
+        //w2 depende de los dígitos repetidos en las cifras del mimso valor
+        
         int posicion = 1;
+        cont++;
         while(max/posicion > 0){
+            w1++;
+            cont+=2;
             int organizador[][] = new int[10][a.length];
+            cont+=3;
             for(int i=0;i<a.length;i++){
+                cont+=3;
                 int j=0;
                 while(organizador[(a[i]/posicion)%10][j] != 0){
+                    w2++;
+                    cont+=7;
                     j++;
                 }
+                cont++;
                 organizador[(a[i]/posicion)%10][j]=a[i];
+                cont+=7;
             }
             int contOrganizados = 0;
+            cont+=3;
             for(int i=0;i<organizador.length;i++){
+                cont+=4;
                 for(int j=0;j<organizador[0].length;j++){
+                    cont+=7;
                     if(organizador[i][j] != 0){
+                        x++;
+                        cont+=5;
                         a[contOrganizados] = organizador[i][j];
                         contOrganizados++;
                     }
                 }
             }
             posicion *= 10;
+            cont+=2;
         }
+        cont++;
+        
+        int n = a.length;
+        int formula = 2+5*x+7*w2+50*w1+81*n*w1;
+        
+        System.out.println("Contador: "+cont);
+        System.out.println("Formula: "+formula+"\n");
+        System.out.print("Arreglo resultante:");
         MostrarArreglo(a);
+        System.out.println("");
     }
     
     public int GetMax(int a[]){
