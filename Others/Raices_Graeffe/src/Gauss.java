b072609ce42531873b39ee08d0c4cd8eb696bf2b
Yuneider <yparada22@gmail.com>
2021-08-23 12:09:51 -0500

    Error fixed

--- Others/Raices_Graeffe/src/Gauss.java
+++ Others/Raices_Graeffe/src/Gauss.java
@@ -1,15 +1,26 @@
 public class Gauss { 
 
-    private void pivote(double matriz[][], int piv, int var) {
+    public void pivote(double matriz[][], int piv, int var) {
         double temp = 0;
         temp = matriz[piv][piv];
         for (int y = 0; y < (var + 1); y++) {
-
-            matriz[piv][y] = matriz[piv][y] / temp;
+            if (matriz[piv][piv]==0){
+                cambiarFilas(piv, matriz);
+                temp = matriz[piv][piv];
+                matriz[piv][y] = matriz[piv][y] / temp;
+            }
+            else{
+                matriz[piv][y] = matriz[piv][y] / temp;
+            }
         }
     }
+    public void cambiarFilas(int a, double matriz[][]){
+        double array[]= matriz[a+1];
+        matriz[a+1]=matriz[a];
+        matriz[a]= array;
+    }
     
-    private void hacerceros(double matriz[][], int piv, int var) {
+    public void hacerceros(double matriz[][], int piv, int var) {
         for (int x = 0; x < var; x++) {
             if (x != piv) {
                 double c = matriz[x][piv];
@@ -19,23 +30,23 @@ public class Gauss {
             }
         }
     }
-    
-    public double[] jordan(double[][] matrix){
+
+    public double[] jordan(double matriz[][]){
         int piv=0;
-        int var=matrix.length;
+        int var= matriz.length;
         double[] sal = new double[var];
         
         for (int a = 0; a < var; a++) {
-            pivote(matrix, piv, var);
-            hacerceros(matrix, piv, var);
+            pivote(matriz, piv, var);
+            hacerceros(matriz, piv, var);
             piv++;
         }
-
-        for (int x = 0; x < var; x++) {
-            sal[x]= matrix[x][var];	
-        }        
         
-        return sal;
+        for (int x = 0; x < var; x++) {
+            sal[x]= matriz[x][var];	
+        }  
+        
+        return sal;    
     }
 
 }
